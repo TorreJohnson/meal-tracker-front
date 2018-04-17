cf31c66b21e41202261c66d51a7dfaafe220e874
TorreJohnson <torre.johnson@gmail.com>
2018-04-16 22:47:28 -0400

    added nutrient counts to food item cards

--- src/components/journal/JournalEntryCards.js
+++ src/components/journal/JournalEntryCards.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Segment, Button, Header, Image, Modal, Card } from "semantic-ui-react";
+import {
+	Segment,
+	Header,
+	Image,
+	Modal,
+	Card,
+	Grid,
+	List
+} from "semantic-ui-react";
 
 class JournalEntryCard extends React.Component {
 	state = {
@@ -50,34 +58,74 @@ class JournalEntryCard extends React.Component {
 									: this.props.foodItem.image
 							}
 						/>
-
 						<Modal.Description>
-							<Header />
-							<p>
-								Logged on{" "}
-								{this.props.foodItem.created_at
-									.split("T")[1]
-									.split(".")[0]
-									.slice(0, 5)}{" "}
-								on {this.props.foodItem.created_at.split("T")[0]}
-								We've found the following gravatar image associated with your
-								e-mail address.
-							</p>
-							<p>Is it okay to use this photo?</p>
+							<Header>Nutrient Values</Header>
+							<Grid columns={2}>
+								<Grid.Column>
+									<List>
+										<List.Item>
+											Beta Carotene: {this.props.foodItem.beta_carotene}
+										</List.Item>
+										<List.Item>
+											Caffeine: {this.props.foodItem.caffeine}
+										</List.Item>
+										<List.Item>
+											Calcium: {this.props.foodItem.calcium}
+										</List.Item>
+										<List.Item>
+											Carbohydrates: {this.props.foodItem.carbohydrate}
+										</List.Item>
+										<List.Item>
+											Cholesterol: {this.props.foodItem.cholesterol}
+										</List.Item>
+										<List.Item>
+											Calories: {this.props.foodItem.calories}
+										</List.Item>
+										<List.Item>Fat: {this.props.foodItem.fat}</List.Item>
+										<List.Item>Fiber: {this.props.foodItem.fiber}</List.Item>
+										<List.Item>
+											Folic Acid: {this.props.foodItem.folic_acid}
+										</List.Item>
+										<List.Item>Iron: {this.props.foodItem.iron}</List.Item>
+										<List.Item>Niacin: {this.props.foodItem.calcium}</List.Item>
+									</List>
+								</Grid.Column>
+								<Grid.Column>
+									<List.Item>
+										Potassium: {this.props.foodItem.potassium}
+									</List.Item>
+									<List.Item>Protein: {this.props.foodItem.protein}</List.Item>
+									<List.Item>
+										Riboflavin: {this.props.foodItem.riboflavin}
+									</List.Item>
+									<List.Item>
+										Sodium: {this.props.foodItem.sodium} Mgs
+									</List.Item>
+									<List.Item>Sugars: {this.props.foodItem.sugars}</List.Item>
+									<List.Item>Thiamin: {this.props.foodItem.thiamin}</List.Item>
+									<List.Item>
+										Vitamin A: {this.props.foodItem.vitamin_a}
+									</List.Item>
+									<List.Item>
+										Vitamin B12: {this.props.foodItem.vitamin_b12}
+									</List.Item>
+									<List.Item>
+										Vitamin C: {this.props.foodItem.vitamin_c}
+									</List.Item>
+									<List.Item>
+										Vitamin D: {this.props.foodItem.vitamin_d}
+									</List.Item>
+									<List.Item>
+										Vitamin E: {this.props.foodItem.vitamin_e}
+									</List.Item>
+									<List.Item>
+										Vitamin K: {this.props.foodItem.vitamin_k}
+									</List.Item>
+									<List.Item>Zinc: {this.props.foodItem.zinc}</List.Item>
+								</Grid.Column>
+							</Grid>
 						</Modal.Description>
 					</Modal.Content>
-					<Modal.Actions>
-						<Button color="black" onClick={this.close}>
-							Nope
-						</Button>
-						<Button
-							positive
-							icon="checkmark"
-							labelPosition="right"
-							content="Yep, that's me"
-							onClick={this.close}
-						/>
-					</Modal.Actions>
 				</Modal>
 			</Segment>
 		);

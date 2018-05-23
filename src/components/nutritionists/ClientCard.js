7fb24127b6fc7cfc5df803cf4e83691d51e08d0b
TorreJohnson <torre.johnson@gmail.com>
2018-05-22 20:26:56 -0400

    refactored and abstracted recommended nutrients in client card

--- src/components/nutritionists/ClientCard.js
+++ src/components/nutritionists/ClientCard.js
@@ -11,40 +11,36 @@ import {
 } from "semantic-ui-react";
 import { sendUserRecNutrients } from "../actions/patchUserActions";
 import { connect } from "react-redux";
+import {
+	recommendedNutrients,
+	snakeCasedNutrients
+} from "../nutrients/NutrientLists";
 import cuid from "cuid";
 
 class ClientCard extends React.Component {
 	state = {
 		assignClientNutrientGoals: false,
-		sodium: 0,
-		rec_beta_carotene: 0,
-		rec_caffeine: 0,
-		rec_calcium: 0,
-		rec_carbohydrate: 0,
-		rec_cholesterol: 0,
-		rec_fat: 0,
-		rec_fiber: 0,
-		rec_folic_acid: 0,
-		rec_iron: 0,
-		rec_niacin: 0,
-		rec_potassium: 0,
-		rec_protein: 0,
-		rec_riboflavin: 0,
-		rec_sodium: 0,
-		rec_sugars: 0,
-		rec_thiamin: 0,
-		rec_vitamin_a: 0,
-		rec_vitamin_b12: 0,
-		rec_vitamin_c: 0,
-		rec_vitamin_d: 0,
-		rec_vitamin_e: 0,
-		rec_vitamin_k: 0,
-		rec_zinc: 0
+		sodium: 0
 	};
 
 	componentDidMount() {
-		this.calculateWeeklyNutrientValues();
+		this.addNutrientsToState(
+			this.createDefaultNutrientObj(recommendedNutrients),
+			this.calculateWeeklyNutrientValues()
+		);
 	}
+
+	createDefaultNutrientObj(nutrientArray) {
+		let nutrientObj = {};
+		for (let i = 0; i < nutrientArray.length; i++) {
+			nutrientObj[nutrientArray[i]] = 0;
+		}
+		return nutrientObj;
+	}
+
+	addNutrientsToState = (nutrientObj, callback = null) => {
+		this.setState(nutrientObj, callback);
+	};
 
 	calculateWeeklyNutrientValues = () => {
 		const items = this.props.client.food_items.filter(
@@ -54,31 +50,7 @@ class ClientCard extends React.Component {
 	};
 
 	mapNutrientCountsInState(items) {
-		let nutrientCounts = {
-			beta_carotene: 0,
-			caffeine: 0,
-			calcium: 0,
-			carbohydrate: 0,
-			cholesterol: 0,
-			fat: 0,
-			fiber: 0,
-			folic_acid: 0,
-			iron: 0,
-			niacin: 0,
-			potassium: 0,
-			protein: 0,
-			riboflavin: 0,
-			sodium: 0,
-			sugars: 0,
-			thiamin: 0,
-			vitamin_a: 0,
-			vitamin_b12: 0,
-			vitamin_c: 0,
-			vitamin_d: 0,
-			vitamin_e: 0,
-			vitamin_k: 0,
-			zinc: 0
-		};
+		let nutrientCounts = this.createDefaultNutrientObj(snakeCasedNutrients);
 		items.forEach(itemObj => {
 			for (let nutrient in itemObj) {
 				if (nutrient === "sodium") {
@@ -122,39 +94,13 @@ class ClientCard extends React.Component {
 		return capitalizedWord.join(" ");
 	}
 
-	nutrients = [
-		"rec_beta_carotene",
-		"rec_caffeine",
-		"rec_calcium",
-		"rec_carbohydrate",
-		"rec_cholesterol",
-		"rec_fat",
-		"rec_fiber",
-		"rec_folic_acid",
-		"rec_iron",
-		"rec_niacin",
-		"rec_potassium",
-		"rec_protein",
-		"rec_riboflavin",
-		"rec_sodium",
-		"rec_sugars",
-		"rec_thiamin",
-		"rec_vitamin_a",
-		"rec_vitamin_b12",
-		"rec_vitamin_c",
-		"rec_vitamin_d",
-		"rec_vitamin_e",
-		"rec_vitamin_k",
-		"rec_zinc"
-	];
-
 	createNutrientValuesListOne = () => {
 		let nutrientValues = [];
 		for (let i = 0; i < 11; i++) {
 			nutrientValues.push(
-				<List.Item>
-					{this.capitalizeNutrientName(this.nutrients[i])}:{" "}
-					{this.state[this.nutrients[i].slice(4)]}
+				<List.Item key={cuid()}>
+					{this.capitalizeNutrientName(recommendedNutrients[i])}:{" "}
+					{this.state[recommendedNutrients[i].slice(4)]}
 				</List.Item>
 			);
 		}
@@ -163,11 +109,11 @@ class ClientCard extends React.Component {
 
 	createNutrientValuesListTwo = () => {
 		let nutrientValues = [];
-		for (let i = 11; i < this.nutrients.length; i++) {
+		for (let i = 11; i < recommendedNutrients.length; i++) {
 			nutrientValues.push(
-				<List.Item>
-					{this.capitalizeNutrientName(this.nutrients[i])}:{" "}
-					{this.state[this.nutrients[i].slice(4)]}
+				<List.Item key={cuid()}>
+					{this.capitalizeNutrientName(recommendedNutrients[i])}:{" "}
+					{this.state[recommendedNutrients[i].slice(4)]}
 				</List.Item>
 			);
 		}
@@ -176,17 +122,17 @@ class ClientCard extends React.Component {
 
 	createRecommendedNutrientForm = () => {
 		let recommendedNutrientForm = [];
-		for (let i = 0; i < this.nutrients.length; i++) {
+		for (let i = 0; i < recommendedNutrients.length; i++) {
 			recommendedNutrientForm.push(
 				<Grid.Column key={cuid()}>
 					<Form.Group widths="equal">
 						<Form.Input
 							fluid
-							label={this.capitalizeNutrientName(this.nutrients[i])}
-							name={this.nutrients[i]}
+							label={this.capitalizeNutrientName(recommendedNutrients[i])}
+							name={recommendedNutrients[i]}
 							type="number"
 							onChange={this.handleNutrientCountChange}
-							placeholder={this.state[this.nutrients[i].slice(4)]}
+							placeholder={this.state[recommendedNutrients[i].slice(4)]}
 						/>
 					</Form.Group>
 				</Grid.Column>
